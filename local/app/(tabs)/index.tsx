a6db65baff043920a318bfa98889719b4d25805f
Mavi-Esquerdo <marciahelenaesquerdo@gmail.com>
2024-08-19 17:46:06 +0000

    saveee

---
+++ local/app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { View, StyleSheet, Platform } from 'react-native';
+
+import Home from './home';
+
+export default function HomeScreen() {
+  return (
+    <View style={jorge.container}>
+      <Home/>
+    </View>
+  ); 
+}
+  const jorge = StyleSheet.create({
+    container:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFF',
+    },
+  })
+
+
